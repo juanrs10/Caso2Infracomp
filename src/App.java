8e9781560743e42164e6d9493f1ddd5322b74f33
karenfbc <111008544+karenfbc@users.noreply.github.com>
2024-10-18 13:22:47 -0500

    feat: clase calcular datos

--- src/App.java
+++ src/App.java
@@ -43,7 +43,7 @@ public class App {
                     String refFileName = scanner.nextLine();
 
                     // Simular el proceso y calcular los datos resultantes (Faltaría implementar la lógica completa)
-                    calcularDatos(numFrames, refFileName);
+                    new CalculadoraDatos(numFrames, refFileName);
                     break;
 
                 case 3:
@@ -88,20 +88,5 @@ public class App {
         }
     }
 
-    // Método para calcular los datos buscados (falla de página, hits, tiempos)
-    public static void calcularDatos(int numFrames, String refFileName) {
-        // Simular el comportamiento de paginación (Faltaría implementar la lógica)
-        // Leer las referencias del archivo y simular el acceso a páginas, contando hits y fallas
-        System.out.println("Simulando el proceso con " + numFrames + " marcos de página y archivo de referencias: " + refFileName);
-        
-        // (1) Simular el comportamiento del proceso cargando las referencias una por una.
-        // (2) Simular el comportamiento del sistema de paginación, incluyendo fallas de página y reemplazo.
-        // (3) Llevar el conteo de hits y fallas de página.
-        
-        // Resultados simulados
-        System.out.println("Resultados simulados: ");
-        System.out.println("Número de fallas de página: X");
-        System.out.println("Porcentaje de hits: Y%");
-        System.out.println("Tiempos de acceso simulados: Z ms");
-    }
+
 }
---
+++ src/CalculadoraDatos.java
@@ -0,0 +1,17 @@
+public class CalculadoraDatos {
+     public CalculadoraDatos(int numFrames, String refFileName) {
+        // Simular el comportamiento de paginación (Faltaría implementar la lógica)
+        // Leer las referencias del archivo y simular el acceso a páginas, contando hits y fallas
+        System.out.println("Simulando el proceso con " + numFrames + " marcos de página y archivo de referencias: " + refFileName);
+        
+        // (1) Simular el comportamiento del proceso cargando las referencias una por una.
+        // (2) Simular el comportamiento del sistema de paginación, incluyendo fallas de página y reemplazo.
+        // (3) Llevar el conteo de hits y fallas de página.
+        
+        // Resultados simulados
+        System.out.println("Resultados simulados: ");
+        System.out.println("Número de fallas de página: X");
+        System.out.println("Porcentaje de hits: Y%");
+        System.out.println("Tiempos de acceso simulados: Z ms");
+    }
+}
